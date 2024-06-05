7d6d1d2f60af1e5db4b5790830ebc3ecb0438e93
Iman Linuxs <student.linuxs@gmail.com>
2024-06-05 10:37:41 +0700

    cover

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/*.{html,js}'
+  ],
+  theme: {
+    extend: {
+     
+      colors : {
+        "border-header"  : '#EEEEEE',
+        "sea-blue"  : '#2F5597'
+        
+        
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
