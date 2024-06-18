e725032e172635f150bfd2f42806a8715f519e0c
iman <student.linuxs@gmail.com>
2024-06-19 04:22:38 +0700

    perbaikan

--- public/js/oceantial.js
+++ public/js/oceantial.js
@@ -49,35 +49,7 @@ var chekbox = function(){
 
 
 
-let burger = document.getElementById("navTrigger"),
-nav = document.getElementById("navMenu");
 
-burger.addEventListener("click", function (e) {
-this.classList.toggle("active");
-nav.classList.toggle("active");
-});
-
-// Get the button
-const menuHead = document.getElementById("menuHead");
-
-// When the user scrolls down 20px from the top of the document, show the button
-
-const scrollFunction = () => {
-if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-) {
-    mybutton.classList.remove("hidden");
-    menuHead.classList.add("bg-default");
-    menuHead.classList.add("drop-shadow-lg");
-
-} else {
-    mybutton.classList.add("hidden");
-    menuHead.classList.remove("bg-default");
-    menuHead.classList.remove("drop-shadow-lg");
-
-}
-};
 
 
 $(document).ready(function(){
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ module.exports = {
         "title2" : '#232020',
         "border2" : '#D2D3D3',
         "btnsosmed" : '#E0DED4',
-        "font-small" : '#6C6C71'
+        "font-small" : '#6C6C71',
+        "font-nav":'#797A7B'
         
         
         
