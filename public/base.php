a1c2c98bf2f19abef050735915338ebadea068fa
iman <student.linuxs@gmail.com>
2024-07-24 21:31:05 +0700

    base per page

--- public/base.php
+++ public/base.php
@@ -2,34 +2,123 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="robots" content="noindex, follow">
-    <meta name="description" content="Isi Descripsi disini" />
-    <meta name="next-head-count" content="8" />
-    <meta name="keywords" content="ISI KEYWORD">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="HandheldFriendly" content="true" />
-    <meta name="apple-touch-fullscreen" content="yes" />
-    <title>Oceantial - Inspired By The Ocean, Crafted For You</title>
 
-    <!-- Facebook Meta Tags -->
-    <meta property="og:url" content="https://ocential.id/">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="Oceantial - Inspired By The Ocean, Crafted For You">
-    <meta property="og:description" content="Isi Descripsi disini">
-    <meta property="og:image" content="https://ocential-push6355610-eb2c50248b11672420d01c5a187f8bed92d51e9cb9b7.gitlab.io/img/logo-ocential.webp">
+    <?php if ($page == 'home') { ?>
+        <meta charset="utf-8">
+        <meta name="robots" content="noindex, follow">
+        <meta name="description" content="Isi Descripsi disini" />
+        <meta name="next-head-count" content="8" />
+        <meta name="keywords" content="ISI KEYWORD">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="HandheldFriendly" content="true" />
+        <meta name="apple-touch-fullscreen" content="yes" />
+        <title>Oceantial - Inspired By The Ocean, Crafted For You</title>
 
-    <!-- Twitter Meta Tags -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta property="twitter:domain" content="ocential.id">
-    <meta property="twitter:url" content="https://ocential.id/">
-    <meta name="twitter:title" content="Oceantial - Inspired By The Ocean, Crafted For You">
-    <meta name="twitter:description" content="Isi Descripsi disini">
-    <meta name="twitter:image" content="https://ocential-push6355610-eb2c50248b11672420d01c5a187f8bed92d51e9cb9b7.gitlab.io/img/logo-ocential.webp">
+        <!-- Facebook Meta Tags -->
+        <meta property="og:url" content="https://ocential.id/">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Oceantial - Inspired By The Ocean, Crafted For You">
+        <meta property="og:description" content="Isi Descripsi disini">
+        <meta property="og:image" content="https://ocential-push6355610-eb2c50248b11672420d01c5a187f8bed92d51e9cb9b7.gitlab.io/img/logo-ocential.webp">
+
+        <!-- Twitter Meta Tags -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta property="twitter:domain" content="ocential.id">
+        <meta property="twitter:url" content="https://ocential.id/">
+        <meta name="twitter:title" content="Oceantial - Inspired By The Ocean, Crafted For You">
+        <meta name="twitter:description" content="Isi Descripsi disini">
+        <meta name="twitter:image" content="https://ocential-push6355610-eb2c50248b11672420d01c5a187f8bed92d51e9cb9b7.gitlab.io/img/logo-ocential.webp">
+    <?php } ?>
 
 
+    <?php if ($page == 'how-it-works') { ?>
+        <meta charset="utf-8">
+        <meta name="robots" content="noindex, follow">
+        <meta name="description" content="Isi Descripsi disini" />
+        <meta name="next-head-count" content="8" />
+        <meta name="keywords" content="ISI KEYWORD">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="HandheldFriendly" content="true" />
+        <meta name="apple-touch-fullscreen" content="yes" />
+        <title>Oceantial - Inspired By The Ocean, Crafted For You</title>
+
+        <!-- Facebook Meta Tags -->
+        <meta property="og:url" content="https://ocential.id/">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Oceantial - Inspired By The Ocean, Crafted For You">
+        <meta property="og:description" content="Isi Descripsi disini">
+        <meta property="og:image" content="https://ocential-push6355610-eb2c50248b11672420d01c5a187f8bed92d51e9cb9b7.gitlab.io/img/logo-ocential.webp">
+
+        <!-- Twitter Meta Tags -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta property="twitter:domain" content="ocential.id">
+        <meta property="twitter:url" content="https://ocential.id/">
+        <meta name="twitter:title" content="Oceantial - Inspired By The Ocean, Crafted For You">
+        <meta name="twitter:description" content="Isi Descripsi disini">
+        <meta name="twitter:image" content="https://ocential-push6355610-eb2c50248b11672420d01c5a187f8bed92d51e9cb9b7.gitlab.io/img/logo-ocential.webp">
+    <?php } ?>
+
+    <?php if ($page == 'faqs') { ?>
+        <meta charset="utf-8">
+        <meta name="robots" content="noindex, follow">
+        <meta name="description" content="Isi Descripsi disini" />
+        <meta name="next-head-count" content="8" />
+        <meta name="keywords" content="ISI KEYWORD">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="HandheldFriendly" content="true" />
+        <meta name="apple-touch-fullscreen" content="yes" />
+        <title>Oceantial - Inspired By The Ocean, Crafted For You</title>
+
+        <!-- Facebook Meta Tags -->
+        <meta property="og:url" content="https://ocential.id/">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Oceantial - Inspired By The Ocean, Crafted For You">
+        <meta property="og:description" content="Isi Descripsi disini">
+        <meta property="og:image" content="https://ocential-push6355610-eb2c50248b11672420d01c5a187f8bed92d51e9cb9b7.gitlab.io/img/logo-ocential.webp">
+
+        <!-- Twitter Meta Tags -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta property="twitter:domain" content="ocential.id">
+        <meta property="twitter:url" content="https://ocential.id/">
+        <meta name="twitter:title" content="Oceantial - Inspired By The Ocean, Crafted For You">
+        <meta name="twitter:description" content="Isi Descripsi disini">
+        <meta name="twitter:image" content="https://ocential-push6355610-eb2c50248b11672420d01c5a187f8bed92d51e9cb9b7.gitlab.io/img/logo-ocential.webp">
+    <?php } ?>
+
+    <?php if ($page == 'about-us') { ?>
+        <meta charset="utf-8">
+        <meta name="robots" content="noindex, follow">
+        <meta name="description" content="Isi Descripsi disini" />
+        <meta name="next-head-count" content="8" />
+        <meta name="keywords" content="ISI KEYWORD">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="HandheldFriendly" content="true" />
+        <meta name="apple-touch-fullscreen" content="yes" />
+        <title>Oceantial - Inspired By The Ocean, Crafted For You</title>
+
+        <!-- Facebook Meta Tags -->
+        <meta property="og:url" content="https://ocential.id/">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Oceantial - Inspired By The Ocean, Crafted For You">
+        <meta property="og:description" content="Isi Descripsi disini">
+        <meta property="og:image" content="https://ocential-push6355610-eb2c50248b11672420d01c5a187f8bed92d51e9cb9b7.gitlab.io/img/logo-ocential.webp">
+
+        <!-- Twitter Meta Tags -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta property="twitter:domain" content="ocential.id">
+        <meta property="twitter:url" content="https://ocential.id/">
+        <meta name="twitter:title" content="Oceantial - Inspired By The Ocean, Crafted For You">
+        <meta name="twitter:description" content="Isi Descripsi disini">
+        <meta name="twitter:image" content="https://ocential-push6355610-eb2c50248b11672420d01c5a187f8bed92d51e9cb9b7.gitlab.io/img/logo-ocential.webp">
+    <?php } ?>
 
     <!-- favicon -->
     <link rel="shortcut icon" href="img/favicon.ico?" type="image/x-icon" />
