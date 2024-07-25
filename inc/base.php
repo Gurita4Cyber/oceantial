b6e96b8488b02802636505b72bf5cdd587c6cfb8
iman <student.linuxs@gmail.com>
2024-07-25 22:44:11 +0700

    Update base.php

--- inc/base.php
+++ inc/base.php
@@ -1,4 +1,4 @@
 <!doctype html>
 <html>
-<base href="/oceantial/" />
-<?php $_SERVER['PUBLIC'] = $_SERVER['DOCUMENT_ROOT'] . '/oceantial/'; ?>
+<base href="/" />
+<?php $_SERVER['PUBLIC'] = $_SERVER['DOCUMENT_ROOT'] . '/'; ?>
