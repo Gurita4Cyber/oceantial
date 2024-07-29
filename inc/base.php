466825d76ec58881fa256c6dedb288a1a0296551
Iman Linuxs <student.linuxs@gmail.com>
2024-07-29 08:06:47 +0700

    Jika terlihat di live hapus
    baca comenan di base.php

--- inc/base.php
+++ inc/base.php
@@ -1,10 +1,15 @@
 <!doctype html>
 <html>
-<base href="/oceantial/" />
-<?php $_SERVER['PUBLIC'] = $_SERVER['DOCUMENT_ROOT'] . '/oceantial/'; ?>
-<?php
-/* untuk di live hosting
 <base href="/" />
 <?php $_SERVER['PUBLIC'] = $_SERVER['DOCUMENT_ROOT'] . '/'; ?>
+<?php
+/* untuk di live hosting hapus oceantialnya
+<base href="/" />
+<?php $_SERVER['PUBLIC'] = $_SERVER['DOCUMENT_ROOT'] . '/'; ?>
+*/
+
+/* untuk di localhost kasih oceantial
+<base href="/oceantial/" />
+<?php $_SERVER['PUBLIC'] = $_SERVER['DOCUMENT_ROOT'] . '/oceantial/'; ?>
 */
 ?>
