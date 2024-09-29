0880f78e995dc2fef141314e8f617e1129627ddb
iman <student.linuxs@gmail.com>
2024-09-29 12:04:22 +0700

    3

--- id/how-it-works.php
+++ id/how-it-works.php
@@ -246,6 +246,12 @@ require('../inc/base.php')
                         </div>
 
                     </div>
+                    <div class="item">
+                        <div class="w-full">
+                            <iframe width="100" height="576" src="https://www.youtube.com/embed/wtQnxSgluQw?si=FPFT-Th8sJ7oUjND" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        </div>
+
+                    </div>
                     <!-- end owl -->
                 </div>
             </div>
